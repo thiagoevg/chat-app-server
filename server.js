3610f791d8f5cf89f811bf8699179736b1be20d1
Thiago Evg <thiago.evg@gmail.com>
2021-05-10 17:37:25 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const Koa = require('koa')
+const http = require('http')
+const socket = require('socket.io')
+const cors = require('cors');
+
+const app = new Koa()
+const server = http.createServer(app.callback())
+const io = socket(server)
+
+app.use(cors());
+
+const SERVER_HOST = 'localhost'
+const SERVER_PORT = process.env.PORT || 8080
+
+io.on('connection', socket => {
+    console.log('[IO] Connection => Server has a new connection')
+    socket.on('chat.message', data => {
+        console.log('[SOCKET] Chat.message => ', data)
+        io.emit('chat.message', data)
+    })
+    socket.on('disconnect', () => {
+        console.log('[SOCKET] Disconnect => A connection was disconnected')
+    })
+})
+
+server.listen(SERVER_PORT, () => {
+    console.log(`[HTTP] Listen => Server is running at port ${SERVER_PORT}`)
+    console.log('[HTTP] Listen => Press CTRL+C to stop it')
+})
